7ce704f4857e7cd6da75063b9150ded2ee2541d2
leecr1215 <leecr1215@naver.com>
2021-07-06 09:14:22 +0900

    finish lecture 3.3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import PropTypes from "prop-types";
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0
   };
@@ -14,7 +18,17 @@ class App extends React.Component {
   minus = () => {
     this.setState(current => ({count: current.count -1}));
   };
+  componentDidMount() { // 처음 함수 실행 시 setstate()->render()->얘 호출됨
+    console.log("component rendered");
+  }
+  componentDidUpdate(){ // 업데이트시 render() 호출 후 호출됨
+    console.log("I just updated");
+  }
+  componentWillUnmount(){ // 창 이동시 render() 호출 후 호출
+    console.log("Goodbye, cruel world");
+  }
   render(){ // java의 main같은 역할
+    console.log("I'm rendering");
     return (<div>
       <h1>The number is:  {this.state.count}</h1>
       <button onClick={this.add}>Add</button>
