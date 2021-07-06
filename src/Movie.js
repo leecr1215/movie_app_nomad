62b0359ab51de9ded90dc0b58950c45acbd22be5
leecr1215 <leecr1215@naver.com>
2021-07-06 23:20:02 +0900

    finish lecture 4.2

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import PropTyps from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}) {
-    return <h2>{title}</h2>
+function Movie({ year, title, summary, poster}) {
+    return (
+    <div class="movies_movie">
+        <div class="movie">
+            <img src={poster} alt={title} title={title} />
+        </div>
+        <div class="movie_data">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <p class="movie_summary">{summary}</p>
+        </div>
+    </div>
+    );
 }
 
 Movie.PropTyps = {
